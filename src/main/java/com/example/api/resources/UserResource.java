a55a5a8445c905d0c01adb32e645d359d1b3839b
Marcos Andrade <marcos_q_andrade@hotmail.com>
2022-07-20 09:39:20 -0300

    endpoint findAll para User.

--- src/main/java/com/example/api/resources/UserResource.java
+++ src/main/java/com/example/api/resources/UserResource.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
@@ -22,5 +24,10 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
         return ResponseEntity.ok().body(mapper.map(userService.findById(id),UserDTO.class));
     }
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok(userService.findAll().stream().map(x -> mapper.map(x,UserDTO.class)).toList());
+    }
+
 
 }
--- src/main/java/com/example/api/services/UserService.java
+++ src/main/java/com/example/api/services/UserService.java
@@ -2,6 +2,9 @@ package com.example.api.services;
 
 import com.example.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
     User findById(Integer id);
+    List<User> findAll();
 }
