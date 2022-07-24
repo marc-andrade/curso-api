dae726824d5540ef71b0f443bc8b9efa0258e138
Marcos Andrade <marcos_q_andrade@hotmail.com>
2022-07-23 21:28:38 -0300

    removendo anotacoes nao usadas

--- src/main/java/com/example/api/resources/exceptions/StandardError.java
+++ src/main/java/com/example/api/resources/exceptions/StandardError.java
@@ -2,13 +2,10 @@ package com.example.api.resources.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class StandardError {
 
